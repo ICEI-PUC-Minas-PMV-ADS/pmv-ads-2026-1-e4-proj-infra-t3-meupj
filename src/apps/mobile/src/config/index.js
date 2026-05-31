764b4ce135b4730941e86d2cd2f8e68fbb97f69b
ericmartins0203 <estevesmartinseric@gmail.com>
2026-05-31 18:08:36 -0300

    merge

--- src/apps/mobile/src/config/index.js
+++ src/apps/mobile/src/config/index.js
@@ -3,6 +3,6 @@
  */
 export const CONFIG = {
   // Altere para o seu IP local se estiver testando em dispositivo físico (ex: http://192.168.1.5:3001)
-  API_URL: 'http://10.0.2.2:3002',
+  API_URL: 'http://localhost:3001',
   TIMEOUT: 10000,
 };
